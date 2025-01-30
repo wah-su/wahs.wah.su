420fdd23a9362e83e11df428b5f25510ae14ed07
Kentai Radiquum <kentai.waah@gmail.com>
2025-01-31 00:51:40 +0500

    round elements

--- src/templates/index.tsx
+++ src/templates/index.tsx
@@ -25,7 +25,7 @@ export default function Index(props: {
         <Header path={props.path} />
         <div className="container mx-auto py-4 px-4 sm:px-8">
           <div>
-            <p className="text-4xl ">Images</p>
+            <p className="text-4xl bg-orange-800/50 rounded-sm p-4">Images</p>
             <div
               id="index_images"
               className="mt-4 flex overflow-x-auto sm:overflow-x-hidden sm:grid sm:grid-cols-[repeat(auto-fill,minmax(25%,1fr))] xl:grid-cols-[repeat(auto-fill,minmax(20%,1fr))] sm:items-center sm:justify-center gap-4"
@@ -35,7 +35,7 @@ export default function Index(props: {
                   <a
                     data-type="index__placeholder__image"
                     key={`index__placeholder__image-${num}`}
-                    className={`relative aspect-square min-w-48 sm:min-w-auto ${
+                    className={`relative aspect-square min-w-48 sm:min-w-auto rounded-sm overflow-hidden ${
                       idx >= 5 ? "hidden xl:block" : ""
                     }`}
                   >
@@ -48,7 +48,7 @@ export default function Index(props: {
               })}
               <a
                 href="/images/"
-                className="text-[#f9ebeb] hover:bg-orange-600 transition-colors aspect-square bg-yellow-950 min-w-48 sm:min-w-auto flex items-center justify-center flex-col"
+                className="text-[#f9ebeb] hover:bg-orange-600 rounded-sm overflow-hidden transition-colors aspect-square bg-yellow-950 min-w-48 sm:min-w-auto flex items-center justify-center flex-col"
               >
                 <span className="material-symbols--arrow-forward-rounded w-16 h-16"></span>
                 <p className="text-xl">All Images</p>
